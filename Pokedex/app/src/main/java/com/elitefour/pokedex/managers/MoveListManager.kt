f039a94562203b604dc8f1418bbe4f1ec04f9f97
Erik Huang <huangti@uw.edu>
2020-06-07 21:59:51 -0700

    move list manager framework

--- Pokedex/app/src/main/java/com/elitefour/pokedex/managers/MoveListManager.kt
+++ Pokedex/app/src/main/java/com/elitefour/pokedex/managers/MoveListManager.kt
@@ -1,7 +1,15 @@
 package com.elitefour.pokedex.managers
 
 import android.content.Context
+import com.elitefour.pokedex.model.Move
+import com.elitefour.pokedex.model.MoveFullInfo
 
 class MoveListManager(context: Context)  {
+    var moveListFullInfoReady: Boolean = false
+    private var moveList: ArrayList<Move> = ArrayList()
+    private var moveFullInfoList: ArrayList<MoveFullInfo> = ArrayList()
 
+    init {
+
+    }
 }
